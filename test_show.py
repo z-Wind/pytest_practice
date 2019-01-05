bc6c831fc87ccc246df5ae22d7f9cc01dbba9bb8
子風 <zpsyhapcst@gmail.com>
2019-01-05 15:23:42 +0800

    Create  test_show.py

---
+++ test_show.py
@@ -0,0 +1,20 @@
+# test_show.py
+import pytest
+
+
+def test_sample1():
+    assert 1 == 1
+
+
+def test_sample2():
+    assert [1, 2, 3] == [3, 2, 1]
+
+
+@pytest.mark.xfail()
+def test_sample3():
+    assert 1 != 2
+
+
+@pytest.mark.xfail()
+def test_sample4():
+    assert 1 == 1
